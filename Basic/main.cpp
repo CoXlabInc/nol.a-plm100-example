d8ab60b8b51aa735a33511e992391b5070de2ece
Jongsoo Jeong <jsjeong@coxlab.kr>
2016-08-23 16:35:07 +0900

    CS-154 Added alarm test code.

--- Basic/main.cpp
+++ Basic/main.cpp
@@ -26,12 +26,21 @@ static void printTask(void *args) {
   printf("[%lu usec] Timer works!\n", micros());
 
   uint16_t year;
-  uint8_t month, day, dayOfWeek, hour, minute, second;
-  System.getDateTime(&year, &month, &day, &dayOfWeek, &hour, &minute, &second);
+  uint8_t month, day, hour, minute, second;
+  RTCCalendar::WeekDay_t dayOfWeek;
+  System.getDateTime(&year, &month, &day, &hour, &minute, &second, &dayOfWeek);
   printf("Now: %u-%u-%u %s %02u:%02u:%02u\n", year, month, day, weekday[dayOfWeek], hour, minute, second);
   printf("Supply voltage: %ld mV\n", System.getSupplyVoltage());
 
   Serial2->printf("[%lu usec] Timer works!\n", micros());
+}
+
+static void eventDateTimeAlarm() {
+  uint16_t year;
+  uint8_t month, day, hour, minute, second;
+  RTCCalendar::WeekDay_t dayOfWeek;
+  System.getDateTime(&year, &month, &day, &hour, &minute, &second, &dayOfWeek);
+  printf("* Alarm! Now: %u-%u-%u %s %02u:%02u:%02u\n", year, month, day, weekday[dayOfWeek], hour, minute, second);
 }
 
 static void buttonPressed() {
@@ -55,7 +64,18 @@ void setup() {
   Serial2->begin(57600);
 
   printf("\n*** [PLM100] Basic Functions ***\n");
-  System.setDateTime(2016, 8, 22, 1, 10, 0, 0);
+  System.setDateTime(2016, 8, 22, 10, 0, 0, RTCCalendar::MONDAY);
+
+  System.onDateTimeAlarm(eventDateTimeAlarm);
+
+  // Set alarm to be occurred every hour when the minute value is changed to 1.
+  System.setDateTimeAlarm(0xFFFF,                     // not supported
+                          0xFF,                       // not supported
+                          0xFF,                       // don't care
+                          0xFF,                       // don't care
+                          1,                          // care
+                          0xFF,                       // not supported
+                          RTCCalendar::UNKNOWNDAY);   // don't care
 
   ledTimer.onFired(ledOnTask, NULL);
   ledTimer.startOneShot(1000);
